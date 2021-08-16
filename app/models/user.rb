196e02bc4f1bb3fe58e1a5154a7c4635aa3298f4
Maxime Haugmard <haugmardmaxime@gmail.com>
2021-08-16 17:50:52 +0200

    rajout has many dans user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :stuffed_animals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
