81de3ba7c40f88deb6f55a8dc75dce9f09f9e441
Maxime Haugmard <haugmardmaxime@gmail.com>
2021-08-16 18:21:17 +0200

    validation user et reservation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :stuffed_animal
-  has_many :reservation
+  has_many :stuffed_animals
+  has_many :reservations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
