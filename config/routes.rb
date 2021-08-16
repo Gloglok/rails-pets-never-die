623be76f2ad306daa8549b49bd9d01eb95fc2457
fidenhan <fidendu2@gmail.com>
2021-08-16 17:37:08 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :stuffed_animals, only: [:index, :show, :new, :create] do
+    resources :reservations, only: [:create]
+  end
+
+  resources :reservations, only: [] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+  resource :dashboard, only: [:show]
 end
