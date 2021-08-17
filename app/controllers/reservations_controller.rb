2ad5042a16b88e0a5f66540a902e7949319c2d4e
fidenhan <fidendu2@gmail.com>
2021-08-17 22:03:30 +0200

    add controller reservations with create accept decline methods

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,20 @@
 class ReservationsController < ApplicationController
+  before_action :find_stuffed_animal, only: [:create]
+  before_action :find_reservation, only: [:accept, :decline]
+
+  def accept
+    @reservation.status = 'Accepted'
+  end
+
+  def decline
+    @reservation.status = 'Declined'
+  end
+
   def create
     @reservation = Reservation.new(reservation_params)
-    @user = User.find(params[:user_id])
-    @stuffed_animal = StuffedAnimal.find(params[:stuffed_animal_id])
-    @reservation.user = @user
+    @reservation.status = 'Pending'
     @reservation.stuffed_animal = @stuffed_animal
+    @reservation.user = current_user
     if @reservation.save
       redirect_to dashboard_path
     else
@@ -13,6 +23,14 @@ class ReservationsController < ApplicationController
   end
 
   private
+
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def find_stuffed_animal
+    @stuffed_animal = StuffedAnimal.find(params[:stuffed_animal_id])
+  end
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date,
