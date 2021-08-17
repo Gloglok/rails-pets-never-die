c8b8ae2a5a1d25f6daa7a1b6143011e29222d045
Louis Duquennoy <louis.duquennoy@gmail.com>
2021-08-17 11:45:21 +0200

    Created StuffedAnimalsController and empty views

---
+++ app/controllers/stuffed_animals_controller.rb
@@ -0,0 +1,31 @@
+class StuffedAnimalsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @stuffed_animals = StuffedAnimal.all
+  end
+
+  def show
+    @stuffed_animal = StuffedAnimal.find(params[:id])
+  end
+
+  def new
+    @stuffed_animal = StuffedAnimal.new
+  end
+
+  def create
+    @stuffed_animal = StuffedAnimal.new(stuffed_animal_params)
+    if @stuffed_animal.save
+      redirect_to stuffed_animal_path(@stuffed_animal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stuffed_animal_params
+    params.require(:stuffed_animal).permit(:name, :species, :user_id, :description,
+                                           :rebirth_date, :price, :weight, :address)
+  end
+end
