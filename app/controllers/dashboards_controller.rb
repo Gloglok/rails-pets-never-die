3e8a6d26263b64be7d7cbdf297e31d4a01e4bf70
arthur bernard <arthur.pierre.bernard@gmail.com>
2021-08-17 18:09:36 +0200

    debut de dashboard avec mon gars sur Louuuuuuiiiiis

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,8 @@
 class DashboardsController < ApplicationController
+  def show
+    @user = current_user
+    @reservations = @user.reservations
+    @stuffed_animals = @user.stuffed_animals
+    @owner_reservations = @user.owner_reservations
+  end
 end
