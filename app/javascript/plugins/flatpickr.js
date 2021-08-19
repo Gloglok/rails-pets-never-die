7043f0c9e3dd2c8935ebec26fe587d06ac1809e8
fidenhan <fidendu2@gmail.com>
2021-08-19 11:55:06 +0200

    fix everything

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,11 +5,13 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 const initFlatpickr = () => {
   const reservationForm = document.getElementById('reservation-form-div');
   if (reservationForm) {
+    const reservations = JSON.parse(reservationForm.dataset.reservations);
     flatpickr("#range_start", {
       plugins: [new rangePlugin({ input: "#range_end" })],
       minDate: "today",
       inline: true,
       dateFormat: "Y-m-d",
+      "disable": reservations
     })
 
     const startDate = document.getElementById("range_start");
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,8 +10,4 @@ class Reservation < ApplicationRecord
   def total_days
     (end_date - start_date).to_i
   end
-
-  def total_price
-    total_days * stuffed_animal.price
-  end
 end
