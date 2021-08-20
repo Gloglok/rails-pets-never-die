4773bc15ebdfd8056308dbad6ad613bb1f2250eb
Louis Duquennoy <louis.duquennoy@gmail.com>
2021-08-20 15:58:54 +0200

    FIIIIIIX

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,8 @@ class DashboardsController < ApplicationController
     @reservations = @user.reservations
     @stuffed_animals = @user.stuffed_animals
     @owner_reservations = @user.owner_reservations
+    p "------------------------------------------------------"
+    p @owner_reservations
+    p "------------------------------------------------------"
   end
 end
